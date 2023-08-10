851748b56518155e80b7e4a4cf91eb052951162c
leogtzr <leogutierrezramirez@gmail.com>
2023-08-10 12:22:18 -0600

    read input from OS Args

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 	"strconv"
 )
 
 func main() {
-	for {
-		fmt.Printf("Enter some text: ")
-		reader := bufio.NewReader(os.Stdin)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "error: at least one argument is required.")
+		os.Exit(1)
+	}
 
-		input, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF { // EOF checking...
-				fmt.Println("EOF detected. Exiting.")
-				os.Exit(0)
-			}
-			fmt.Println("Error reading input:", err)
-			return
-		}
+	input := os.Args[1]
 
-		input = input[:len(input)-1]
-
-		number, err := strconv.Atoi(input)
-		if err != nil {
-			fmt.Println("Error converting input to int:", err)
-		} else {
-			output := numToWord(number)
-			fmt.Printf("%d -> '%s'\n", number, output)
-		}
+	number, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println("Error converting input to int:", err)
+	} else {
+		output := numToWord(number)
+		fmt.Printf("%d -> '%s'\n", number, output)
 	}
 }
